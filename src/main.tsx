8f58c07fb9e2053526d9fbca670f684bc8c317a2
Vincent <vincenttayros078@gmail.com>
2026-03-02 14:23:10 +0800

    added ok

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { 
   faChartPie, 
